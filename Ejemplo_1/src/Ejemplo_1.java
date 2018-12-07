380ba181db794c3571a8220f362c03d1de8df6c7
EntornosGrupal <45687693+EntornosGrupal@users.noreply.github.com>
2018-12-07 13:22:53 +0100

    JAVI PRIMER COMMIT

---
+++ Ejemplo_1/src/Ejemplo_1.java
@@ -0,0 +1,15 @@
+
+public class Ejemplo_1 {
+	public static void main(String[] args) {
+		for (int x=0; x<10; x++) {
+			System.out.print("Hola");
+		}
+		int Contador=0;
+		while (Contador<10) {
+			
+			Contador= Contador+1;
+			System.out.print(Contador);
+		}
+		System.out.print("Hola2");
+	}
+}
